5a70e43e7f8ec935b91a71346c45bc7fed4622f5
jeremybosqui <bosdani97@gmail.com>
2022-02-24 13:44:40 -0400

    summary js btn + add css btn

--- Scripts/Home/scroll.js
+++ Scripts/Home/scroll.js
@@ -1,4 +1,18 @@
 'use strict'
 /*====================================================*/
 
-export default class Scroll 
+export default class Scroll {
+    // ajout du scroll qui permet de ramener l'utilisateur vers le contenu
+    scrollBtn () {
+        window.addEventListener ("scroll", () => {
+            let button = document.getElementById('photographersLink');
+            let y = window.scrollY;
+            if (y >= 130) {
+                button.style.display = "block";
+            } else {
+                button.style.display="none";
+            }
+        });
+    }
+
+}
