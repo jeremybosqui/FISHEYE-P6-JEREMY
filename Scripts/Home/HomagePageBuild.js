f71459470f84b54c4684589e87ae9fdc70fb6bcd
jeremybosqui <bosdani97@gmail.com>
2022-02-24 13:38:41 -0400

    summary html + js update

---
+++ Scripts/Home/HomagePageBuild.js
@@ -0,0 +1,32 @@
+'use strict'
+/*====================================================*/
+
+
+// utilisation d'export default pour n'exporter qu'un seul objet ou variable qui est le builder de la page d'accueil
+export default class HomePageBuild {
+        // mise en place du builder de la section photographer plus appel de la fonction "filtertag" et egalement celui du bouton "passer au contenu" sur la page d'accueil
+    buildPhotographers (data) {
+        let photographers = data.photographers;
+        photographers.map(photographe => {
+            let sectionPhotographers = document.getElementById('photographers');
+            let articlePhotographers = document.createElement('article');
+            articlePhotographers.className = photographe.tags.join('') + 'articlePh';
+            let templatePhotographer = `
+            <a href ="photographers.html?id=${photographe.id}" title="${photographe.name}">
+                <img src="${photographe.portrait}" alt"${photographe.alt}">
+                <h2 class"name">${photographe.name}</h2>
+            </a>
+            
+            
+            
+            
+            
+            
+            
+            
+            `
+        })
+
+
+    }
+}
