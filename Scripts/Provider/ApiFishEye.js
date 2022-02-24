92cc471cb77a63bb055697e8e1efad5f766f19da
jeremybosqui <bosdani97@gmail.com>
2022-02-24 12:35:02 -0400

    summary

---
+++ Scripts/Provider/ApiFishEye.js
@@ -0,0 +1,17 @@
+'use strict'
+
+export default class ApiFishEye {
+    async getDataFishEye() {
+        const url = 'Api/FishEye/photographers.json';
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const dataPhotographers = [...data.photographers];
+        const dataMedias = [ ...data.media];
+
+        return {
+            'photographers' : dataPhotographers,
+            'media' : dataMedias
+        };
+    }
+}
