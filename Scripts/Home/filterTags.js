eb425bf6c78b66c1fbd6614d97796416d591b1e1
jeremybosqui <bosdani97@gmail.com>
2022-02-24 21:35:24 -0400

    summary

---
+++ Scripts/Home/filterTags.js
@@ -0,0 +1,53 @@
+'use strict'
+
+/*================================================*/
+/*======  mise en place de la partie filtre avec les tags present dans la nav =============*/
+export default class Filter {
+
+    filterTags () {
+        let filtres = document.querySelector('ul');
+        let articles = document.querySelectorAll('.articlePh');
+
+        // initialiser l'event listener on click
+        filtres.addEventListener('click', event => {
+            let classValue = event.target.classList.value;
+            // active ou desactive la classe active
+            if (-1 === classValue.indexOf('actived')) {
+                event.target.classList.add('actived')
+            } else {
+                event.target.classList.remove('actived')
+            }
+
+            this.sortDomArticle(articles);
+        })
+    }
+
+    // retravailler les filtres avec la classe active et creer "filterselected"
+
+    getActiveFilters() {
+        let currentFilters = document.querySelectorAll('ul li.actived');
+        let filterSelected = [];
+
+        currentFilters.forEach(function (currentFilter) {
+            filterSelected.push(currentFilter.getAttribute("data-filter"));
+        });
+
+        return filterSelected;
+    }
+
+    // comparer / verifier si "filters" a la meme valeur que la classe "article"
+
+    ownAllFilters(article) {
+        let filters = this.getActiveFilters();
+        let classValue = article.classList.value;
+        let classes = classValue.split(' ');
+        let intersection = filters.filter(
+            x => classes.includes(x)
+        );
+
+        return filters.length == intersection.length;
+    }
+
+
+
+}
--- Scripts/Home/scroll.js
+++ Scripts/Home/scroll.js
@@ -3,14 +3,14 @@
 
 export default class Scroll {
     // ajout du scroll qui permet de ramener l'utilisateur vers le contenu
-    scrollBtn () {
+    scrollBtn () { // initier la fonction scroll pour que le bouton apparaisse quand l'utilisateur scroll jusqu'à une certaine hauteur de son ecran et sinon il disparait "display block" et "display none"
         window.addEventListener ("scroll", () => {
             let button = document.getElementById('photographersLink');
             let y = window.scrollY;
             if (y >= 130) {
-                button.style.display = "block";
+                button.style.display = "block"; // apparait une fois arriver a une hauteur vertical "Y"
             } else {
-                button.style.display="none";
+                button.style.display="none"; // sinon disparait
             }
         });
     }
